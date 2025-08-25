1e87e0b23c7048fb02701307b2eb3f500818940b
Aryan Chogale <128447275+Aryan07CH@users.noreply.github.com>
2025-08-25 16:45:47 +0530

    Add files via upload

---
+++ C_Prog/linkedlist.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+struct node{
+	int data;
+struct node *next;
+struct node *prev;
+};
+
+int main(){
+	struct node f,*ptr,*pre;
+
+//	f.data=45;
+//	f.next=NULL;
+//	a.data=20;
+//	a.next=NULL;
+//	b.data=30;
+//	b.next=NULL;
+//	c.data=40;
+//	c.next=NULL;
+//	f.next=&a;
+//	a.next=&b;
+//	b.next=&c;
+//	ptr=&f;
+//	
+//	while(ptr!=NULL){
+//		printf("\n %d",ptr->data);
+//		ptr=ptr->next;
+//	}
+
+	
+//	for()
+	
+	return 0;
+}
