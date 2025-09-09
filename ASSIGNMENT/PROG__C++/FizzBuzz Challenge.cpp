eeaa474923b6fe9778abfbe39286c2183a89f9be
Aryan Chogale <128447275+Aryan07CH@users.noreply.github.com>
2025-09-09 18:50:17 +0530

    Update FizzBuzz Challenge.cpp

--- ASSIGNMENT/PROG__C++/FizzBuzz Challenge.cpp
+++ ASSIGNMENT/PROG__C++/FizzBuzz Challenge.cpp
@@ -6,13 +6,14 @@ using namespace std;
 int main(){
 	for(int i=1;i<=50;i++){
 		
-		if(i%3==0)
-			cout<<"\nFizz ";
-		else if(i%5==0)
-			cout<<"\nBuzz ";
-		else if(i%3==0 && i%5==0)
+		if(i%3==0 && i%5==0)
 			cout<<"\nFizzBuzz ";
+		else if(i%3==0)
+			cout<<"\nBuzz ";
+		else if(i%5==0)
+			
 		else
 		cout<<endl<<i;
 	}
 }
+
