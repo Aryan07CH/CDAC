f970e69f00a09d9e49e714eef1bed3b3dc4bc875
Aryan Chogale <128447275+Aryan07CH@users.noreply.github.com>
2025-09-21 22:43:57 +0530

    Update armstrong_num.cpp

--- Prog_C++/9-9-2025/armstrong_num.cpp
+++ Prog_C++/9-9-2025/armstrong_num.cpp
@@ -1,10 +1,9 @@
 #include<iostream>
 #include<cmath>
-//reverse a number digitwise
+
 using namespace std;
 int main()
 {
-	/*
 	int num,digit=0,sum=0;
 	cout<<"\nEnter a number:";
 	cin>>num;
@@ -21,8 +20,8 @@ int main()
 		cout<<endl<<num1<<" Is Armstrong Number ";
 	else
 		cout<<endl<<num1<<" It is not";
-	*/
 	
-	//    Armstrong numbers between 1 and 1000 
+
 	
 }
+
